fdef43b19e0495f2defe7c60623fe291af9e7e22
Илья <qq.ferveks@gmail.com>
2021-04-06 18:43:17 +0300

    2. Создать контейнер на базе связанного списка [#428739]

--- src/main/java/сollection/SimpleLinkedList.java
+++ src/main/java/сollection/SimpleLinkedList.java
@@ -1,29 +1,82 @@
 package сollection;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
 
-    private Node fist;
-    private Node last;
-    private int size;
-    private int modeCount;
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+    private int modeCount = 0;
 
     @Override
     public void add(E value) {
-
+        Node<E> node = last;
+        Node<E> newNode = new Node<>(node, value, null);
+        last = newNode;
+        if (node == null) {
+            first = newNode;
+        } else {
+            node.next = newNode;
+        }
+        size++;
+        modeCount++;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        Node<E> node;
+        if (index < size && index < (size / 2)) {
+            node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+            return node.item;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
-    }
-    private class Node {
+        return new Iterator<E>() {
+            int failFast = modeCount;
+            int position = 0;
 
+            @Override
+            public boolean hasNext() {
+                return size > position;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (failFast != position) {
+                    throw new ConcurrentModificationException();
+                }
+                return get(position++);
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
