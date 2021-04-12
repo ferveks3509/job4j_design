5e8b728166ac8facee6e1e95103bb154f6d77ef5
Илья <qq.ferveks@gmail.com>
2021-04-12 18:44:29 +0300

    5. Очередь на двух стеках [#428740]

--- src/main/java/сollection/SimpleStack.java
+++ src/main/java/сollection/SimpleStack.java
@@ -2,12 +2,19 @@ package сollection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size;
 
     public T pop() {
         T value = linked.deleteFirst();
+        size--;
         return value;
     }
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
