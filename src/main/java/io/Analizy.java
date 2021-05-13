c3404748fc38b01e0cdc4b3613652ec5495ecc8b
Илья <qq.ferveks@gmail.com>
2021-05-13 18:04:34 +0300

    2. Анализ доступности сервера. [#428793]

--- src/main/java/io/Analizy.java
+++ src/main/java/io/Analizy.java
@@ -5,26 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
+    public List<String> unavailable(String source) {
         List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             String line;
+            boolean flag = false;
             while ((line = in.readLine()) != null) {
                 String status = line.split(" ")[0];
                 String time = line.split(" ")[1];
                 if (status.contains("400") || status.contains("500")) {
                     rsl.add(time + ";");
+                    flag = true;
                 }
                 if (status.contains("200") || status.contains("300")) {
                     rsl.add(time + System.lineSeparator());
+                    flag = false;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (String el : rsl) {
-                out.write(el);
+        return rsl;
+    }
+    public void save(List<String> list, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String el : list) {
+                out.println(el);
             }
         } catch (Exception e) {
             e.printStackTrace();
