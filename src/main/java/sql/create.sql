336235300d04c2791d4c1958d5481f4b3e3bb8c3
Илья <qq.ferveks@gmail.com>
2021-07-02 11:31:49 +0300

    1. Схема прав пользователи и роли. [#1093]

--- src/main/java/sql/create.sql
+++ src/main/java/sql/create.sql
@@ -1,11 +1,11 @@
+create table role(
+id serial primary key,
+description text
+);
 create table user(
 id serial primary key,
 name varchar(255),
 role_id int references role(id)
-);
-create table role(
-id serial primary key,
-description text
 );
 create table rules(
 id serial primary key,
@@ -13,8 +13,8 @@ name text
 );
 create table item(
 id serial primary key,
-description_role text references role(descriptions),
-name_rules text references rules(name),
+role_id text references role(id),
+rules_id text references rules(id),
 name_id int references user(id),
 category_id int references category(id),
 state_id int references state(id)
