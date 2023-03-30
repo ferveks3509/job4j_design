36184079c4aadb0d5e59164eb56e91456754909b
ferveks3509 <qq.ferveks@gmail.com>
2023-03-30 08:53:10 +0300

    algo

--- src/main/java/algo/StringCompare.java
+++ src/main/java/algo/StringCompare.java
@@ -5,13 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
         int minLength = Math.min(o1.length(), o2.length());
         for (int i = 0; i < minLength; i++) {
-            rsl = Character.compare(o1.charAt(i), o2.charAt(i));
-            if (rsl != 0)
-                break;
+            if (o1.charAt(i) != o2.charAt(i))
+                return Character.compare(o1.charAt(i), o2.charAt(i));
         }
-        return rsl != 0 ? rsl : Integer.compare(o1.length(), o2.length());
+        return Integer.compare(o1.length(), o2.length());
     }
 }
