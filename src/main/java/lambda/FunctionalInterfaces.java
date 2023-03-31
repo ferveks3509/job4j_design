16eaac3039e64bef32cd599a08191821fd8b4b68
ferveks3509 <qq.ferveks@gmail.com>
2023-03-31 15:37:24 +0300

    Функциональный интерфейс

---
+++ src/main/java/lambda/FunctionalInterfaces.java
@@ -0,0 +1,30 @@
+package lambda;
+
+import java.util.*;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (numb, value) -> map.put(numb, value);
+
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+
+        BiPredicate<Integer, String> biPred = (i, s) -> (i % 2 == 0) || (s.length() == 4);
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        Consumer<String> con = s -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
+        List<String> strings = Arrays.asList("a", "b", "c", "d");
+        for (String s : strings) {
+            con.accept(func.apply(s));
+        }
+    }
+}
