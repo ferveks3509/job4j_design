7ff1f502f80b9a017274f3de842ecf9fd9a16564
ferveks3509 <qq.ferveks@gmail.com>
2023-04-01 14:12:25 +0300

    Преобразование List в Map

--- src/main/java/lambda/stream/School.java
+++ src/main/java/lambda/stream/School.java
@@ -2,6 +2,7 @@ package lambda.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,5 +11,14 @@ public class School {
         return students.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Student> convert(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        student -> student,
+                        (el1, el2) -> el1
+                ));
     }
 }
