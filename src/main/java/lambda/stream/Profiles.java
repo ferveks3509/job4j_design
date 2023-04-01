285e5162c33b27dd9cfc9c8a9d108875ee6e4df7
ferveks3509 <qq.ferveks@gmail.com>
2023-04-01 13:49:48 +0300

    Список адресов

---
+++ src/main/java/lambda/stream/Profiles.java
@@ -0,0 +1,13 @@
+package lambda.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+}
