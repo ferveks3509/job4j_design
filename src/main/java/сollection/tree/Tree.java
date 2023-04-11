f0c29b9813f4c190a83c67f79d6546db806e8022
ferveks3509 <qq.ferveks@gmail.com>
2023-04-11 07:23:21 +0300

    Создать элементарную структуру дерева [#1711 #157257]

---
+++ src/main/java/сollection/tree/Tree.java
@@ -0,0 +1,20 @@
+package сollection.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface Tree<E> {
+    boolean add(E parent, E children);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
