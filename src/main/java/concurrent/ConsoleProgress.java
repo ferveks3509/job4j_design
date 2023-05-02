013c40738043b60ec74be174ef1f534e5f237850
ferveks3509 <qq.ferveks@gmail.com>
2023-05-02 19:05:51 +0300

    Прерывание нити

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var pr = new String[] {"\\", "|", "/"};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                for (String el : pr) {
+                    System.out.print("\rLoading: " + el);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        public static void main (String[]args) throws InterruptedException {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+        }
+    }
