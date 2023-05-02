e0f50c5af115f758962da74384e6c10c59d20cfd
ferveks3509 <qq.ferveks@gmail.com>
2023-05-02 15:46:33 +0300

    create and use Thread

---
+++ src/main/java/concurrent/ConcurrentOutput.java
@@ -0,0 +1,15 @@
+package concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
