6799f5b453f60da97a2320bc64d43bf2c1bad9a6
ferveks3509 <qq.ferveks@gmail.com>
2023-05-05 11:05:31 +0300

    JCIP. Настройка библиотеки

--- src/main/java/concurrent/atomic/Count.java
+++ src/main/java/concurrent/atomic/Count.java
@@ -1,12 +1,17 @@
 package concurrent.atomic;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
     private int count;
 
     public synchronized void increment() {
         count++;
     }
-    public int get() {
+    public synchronized   int get() {
         return count;
     }
 }
