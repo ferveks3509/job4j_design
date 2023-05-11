bb67ffed7674f5ae2549c3535694fbee93f9055e
ferveks3509 <qq.ferveks@gmail.com>
2023-05-11 21:19:33 +0300

    arr revers

---
+++ src/main/java/algo/TurnBack.java
@@ -0,0 +1,13 @@
+package algo;
+
+public class TurnBack {
+    public static int[] back(int[] array) {
+        for (int index = 0; index < array.length / 2; index++) {
+            int temp = array[index];
+            array[index] = array[array.length - index - 1];
+            array[array.length - index - 1] = temp;
+        }
+        return array;
+    }
+}
+
