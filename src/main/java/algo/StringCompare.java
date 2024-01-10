161868c68f4513cea868a386598c5d5e6a6ac012
ilya <qq.ferveks@gmail.com>
2024-01-10 10:45:21 +0300

    try

--- src/main/java/algo/StringCompare.java
+++ src/main/java/algo/StringCompare.java
@@ -8,6 +8,7 @@ public class StringCompare implements Comparator<String> {
         int minLength = Math.min(o1.length(), o2.length());
         for (int i = 0; i < minLength; i++) {
             if (o1.charAt(i) != o2.charAt(i))
+                System.out.println("hi hi");
                 return Character.compare(o1.charAt(i), o2.charAt(i));
         }
         return Integer.compare(o1.length(), o2.length());
